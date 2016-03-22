7f3a79a10591e4ece8ab949c6263f0f87223626f
=SimHub <lackmann.simon@gmail.com>
2016-03-22 20:44:53 +0100

    modified:index.html

--- js/creative.js
+++ js/creative.js
@@ -4,11 +4,11 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-(function($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
@@ -23,7 +23,7 @@
     })
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
     });
 
@@ -44,5 +44,25 @@
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
+
+    //BADGES
+
+    var badges = ["http-1", "badges_githubBasics_Stage01",
+        "badges_eCommerce_Stage5",
+        "badges_githubBasics_Stage01",
+        "badges_JavaScript_ExpressBasics_Stage3",
+        "badges_JavaScript_MEANapp_Stage2",
+        "badges_JavaScript_nodeBasics_Stage1",
+        "badges_JavaScript_npmbasics_Stage2",
+        "badges_Mongo_Basics_Stage2",
+        "badges_phpstandards_Stage1",
+        "badges_python_basics_stage5_alt",
+        "badges_ruby_basics_Stage1",
+        "badges_WP_BizOwners_Stage1",
+        "badges-js-gulp-stage2"];
+
+    for (var i = 0; i < badges.length; i++) {
+        $('#badges').append('<img src="img/badges/' + badges[i] + '.png" width="40" alt="">')
+    }
 
 })(jQuery); // End of use strict
